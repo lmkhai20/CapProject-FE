e60e932609faec67043a5ddba9314d9b21f179bc
lmkhai20 <leminhkhai2000@gmail.com>
2023-12-02 00:21:16 +0700

    fixUI

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,43 +1,33 @@
 // import react from 'react'
-import imgg from '../../assets/images/aaa.webp'
-import './pages.scss'
+import imgg from "../../assets/images/aaa.webp";
+import imgg2 from "../../assets/images/readcccd.png";
+import "./pages.scss";
 
 const Home = () => {
-    return (
-        <div className='home-container'>
-            <div className='home-main'>
-                <div className='home-main-right'>
-                    <img className='main-img' src={imgg} alt='imgg' />
-                    <div className='text-show'>
-                        <p id='name-denoise'>IDENTITY CARD </p>
-                        <p id='name-100'>100% Automatically and <span>Free</span> </p>
-                    </div>
-
-                </div>
-                <div className='home-main-left'>
-
-                    <div className='upload'><a href='/reader' className="btn-upload">
-                        Upload Image
-                    </a >
-                    </div>
-                    <div className='home-left-bot'>
-                        <div className='left-bot-p'>
-                            <p>No image?</p>
-                            <p>Try one of these:</p>
-                        </div>
-                        <img className='left-bot-img' alt='Upload' src='https://static.remove.bg/uploader-examples/person/2_thumbnail.jpg'></img>
-                        <img className='left-bot-img' alt='Upload' src='https://static.remove.bg/uploader-examples/animal/5_thumbnail.jpg'></img>
-                        <img className='left-bot-img' alt='Upload' src='https://static.remove.bg/uploader-examples/car/3_thumbnail.jpg'></img>
-                        <img className='left-bot-img' alt='Upload' src='https://static.remove.bg/uploader-examples/product/9_thumbnail.jpg'></img>
-                    </div>
-                    
-
-                </div>
-            </div>
-            
-            
-
+  return (
+    <div className="home-container">
+      <div className="home-main">
+        <div className="home-main-right">
+          <img className="main-img" src={imgg} alt="imgg" />
+          <div className="text-show">
+            <p id="name-denoise">IDENTITY CARD </p>
+            <p id="name-100">
+              100% Automatically and <span>Free</span>{" "}
+            </p>
+          </div>
         </div>
-    )
-}
-export default Home
+        <div className="home-main-left">
+          <div className="upload">
+            <a href="/reader" className="btn-upload">
+              Read Card
+            </a>
+          </div>
+          <div >
+            <img className="read-img" src={imgg2} alt="read" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Home;
